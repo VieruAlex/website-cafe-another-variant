1a10b8e11c9ab15d1e30e815da4df0c3c31239e8
Alex Vieru <148143451+VieruAlex@users.noreply.github.com>
2024-04-29 15:36:36 +0300

    final

---
+++ dist/second.94358b24.js
@@ -0,0 +1,20 @@
+function show() {
+    var p = document.getElementById("pwd");
+    p.setAttribute("type", "text");
+}
+function hide() {
+    var p = document.getElementById("pwd");
+    p.setAttribute("type", "password");
+}
+var pwShown = 0;
+document.getElementById("eye").addEventListener("click", function() {
+    if (pwShown == 0) {
+        pwShown = 1;
+        show();
+    } else {
+        pwShown = 0;
+        hide();
+    }
+}, false);
+
+//# sourceMappingURL=second.94358b24.js.map
